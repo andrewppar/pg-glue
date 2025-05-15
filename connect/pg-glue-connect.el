85f0804ed9fa27d73035da7a9c4e12ac80f020da
Andrew Parisi <anparisi@cisco.com>
2025-05-15 09:40:27 -0500

    connection updates: timeout and erroring

--- connect/pg-glue-connect.el
+++ connect/pg-glue-connect.el
@@ -12,12 +12,19 @@
 (defvar pg-glue/connection nil
   "The database connection object.")
 
+(defvar pg-glue-connection/query-timeout 10000
+  "The number of seconds before a query will give up and return an error.")
+
+
 (defun pg-glue-connect/set!
     (database username password server port &optional ssl?)
   "Set the connection with DATABASE, USERNAME, PASSWORD, SERVER and PORT.
 Optionally pass SSL? to use an ssl connection."
-  (when pg-glue/connection
-    (pg-disconnect pg-glue/connection))
+  (unwind-protect
+       (when pg-glue/connection
+	 (pg-disconnect pg-glue/connection))
+    (setq pg-glue/connection nil))
+  (setq pg-read-timeout pg-glue-connection/query-timeout)
   (setq pg-glue/connection
 	(pg-connect database username password server port ssl?)))
 
